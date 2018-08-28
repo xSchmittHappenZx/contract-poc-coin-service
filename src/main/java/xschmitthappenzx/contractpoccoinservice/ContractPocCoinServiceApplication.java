50e59c4fc8fff6498b5db75c31287afbda5851b3
xSchmittHappenZx <calebschmitt12@gmail.com>
2018-08-27 20:03:13 -0500

    Project: Init

---
+++ src/main/java/xschmitthappenzx/contractpoccoinservice/ContractPocCoinServiceApplication.java
@@ -0,0 +1,12 @@
+package xschmitthappenzx.contractpoccoinservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContractPocCoinServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContractPocCoinServiceApplication.class, args);
+	}
+}
---
+++ src/test/java/xschmitthappenzx/contractpoccoinservice/ContractPocCoinServiceApplicationTests.java
@@ -0,0 +1,16 @@
+package xschmitthappenzx.contractpoccoinservice;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ContractPocCoinServiceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
